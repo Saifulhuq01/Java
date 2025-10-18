ffdc2b4db87f951f09ad31219086a8134893f2e4
saifulhuq01 <s.md.saifulhuq007@gmail.com>
2025-10-18 21:52:27 +0530

    interface v3

--- interfaceSample/interfacedemo2.java
+++ interfaceSample/interfacedemo2.java
@@ -2,10 +2,15 @@ package interfaceSample;
 
 interface Innerinterfacedemo2 {
     public void test(); 
+    // it can have only abstract methods
+    abstract void display();
     
 }
 public class interfacedemo2 implements Innerinterfacedemo2{
-    public void test(){
+    public void  test(){
         System.out.println("im in interfacedemo2");
+    }
+    public void display(){
+        System.out.println("display method in interfacedemo2");
     }
 }
